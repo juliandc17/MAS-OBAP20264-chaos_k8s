1c6aa36b9959e5a3b82a8fada48eb76ddfa8f397
Juliandc17 <julian_dc17@hotmail.com>
2026-08-27 23:45:20 -0500

    fix: configuración final para cuenta GCP completa - e2-standard-4 3 nodos

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -64,11 +64,11 @@ setup_gke_cluster() {
     log "Creando cluster GKE $CLUSTER_NAME..."
     gcloud container clusters create $CLUSTER_NAME \
       --zone=$GCP_REGION \
-      --num-nodes=1 \
-      --machine-type=e2-medium \
+      --num-nodes=3 \
+      --machine-type=e2-standard-4 \
       --enable-autoscaling \
       --min-nodes=1 \
-      --max-nodes=2 \
+      --max-nodes=5 \
       --disk-size=30GB \
       --enable-network-policy \
       --workload-pool="${GCP_PROJECT}.svc.id.goog" \
